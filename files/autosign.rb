8d06aa6b4afd9f28c86e15351929ef6fdc2077e8
Eric Sorenson <eric.sorenson@puppetlabs.com>
2014-09-22 23:24:34 -0700

    Updates to autosign.rb
    Use ruby from the environment, not necessarily /opt/puppet/bin
    Adds a PSK check and move it to the top to avoid hitting the
      network unnecessarily

--- files/autosign.rb
+++ files/autosign.rb
@@ -1,9 +1,11 @@
-#!/opt/puppet/bin/ruby
+#!/usr/bin/env ruby
 
 require 'etc'
 
 ENV['HOME'] = Etc.getpwuid(Process.uid).dir
 ENV['FOG_RC'] = '/etc/puppetlabs/puppet/autosignfog.yaml'
+
+my_psk = 'BEC02265-DF93-4E8A-B22A-8C24354E9409'
 
 require 'fog'
 require 'puppet'
@@ -17,12 +19,20 @@ instance_id = pp_instance_id['value']
 
 retcode = 0
 
+pp_preshared_key = csr.request_extensions.find{ |a| a['oid'] == 'pp_preshared_key' } 
+
+# first some local checks that don't require a hitting the API at all
+if pp_preshared_key.nil? or pp_preshared_key != my_psk
+  retcode = 4
+  exit retcode
+elsif csr.name != clientcert
+  retcode = 1
+end
+
 ec2 = Fog::Compute.new( :provider => :aws)
 server = ec2.servers.find { |s| s.id == instance_id }
 
-if csr.name != clientcert
-  retcode = 1
-elsif not server
+if not server
   retcode = 2
 elsif server.state != 'running'
   retcode = 3
