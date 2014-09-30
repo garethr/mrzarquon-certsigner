25d80b61d3515d70f41620fd4334b39c35fe2e2a
Gareth Rushgrove <gareth@morethanseven.net>
2014-09-30 15:56:52 +0100

    lots of logging

--- files/puppet-aws-autosign.rb
+++ files/puppet-aws-autosign.rb
@@ -8,17 +8,30 @@ ENV['FOG_RC'] = '/etc/puppet/autosignfog.yaml'
 require 'fog'
 require 'puppet'
 require 'puppet/ssl/certificate_request'
+require 'securerandom'
+
+uuid = SecureRandom.uuid
+
+log = Logger.new("/tmp/#certsigner-{uuid}.log")
 
 clientcert = ARGV.pop
+
+log.info(clientcert)
 
 csr = Puppet::SSL::CertificateRequest.from_s(STDIN.read)
 pp_instance_id = csr.request_extensions.find { |a| a['oid'] == 'pp_instance_id' }
 instance_id = pp_instance_id['value']
 
+log.info(STDIN.read)
+log.info(pp_instance_id)
+log.info(instance_id)
+
 retcode = 0
 
 ec2 = Fog::Compute.new( :provider => :aws)
 server = ec2.servers.find { |s| s.id == instance_id }
+
+log.info(server.id)
 
 #if csr.name != clientcert
 #  retcode = 1
@@ -29,5 +42,7 @@ if not server
 elsif server.state != 'running'
   retcode = 3
 end
+
+log.info(retcode)
 
 exit retcode
